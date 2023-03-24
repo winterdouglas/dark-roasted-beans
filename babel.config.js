96f48e0bc5ae1a339fabbf60561a0af31ad050c5
Douglas Winter <a.winterdoug@gmail.com>
2023-03-24 12:09:46 +0100

    Setting up tooling

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [
+          ".ios.js",
+          ".android.js",
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+          ".json",
+        ],
+        alias: {
+          "@components": "./src/components",
+        },
+      },
+    ],
+  ],
 };
